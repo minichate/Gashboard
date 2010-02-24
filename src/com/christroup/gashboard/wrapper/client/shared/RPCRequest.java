54fae89dbdce2f219f9df76581a4b09743857f66
minichate <minichate@5e9b253a-1a51-11df-85e6-9b4bd7dd5138>
2010-02-24 13:22:03 +0000

    add cross-domain RPC support via proxy mechanism
    git-svn-id: https://gashboard.googlecode.com/svn/trunk@22 5e9b253a-1a51-11df-85e6-9b4bd7dd5138

---
+++ src/com/christroup/gashboard/wrapper/client/shared/RPCRequest.java
@@ -0,0 +1,18 @@
+package com.christroup.gashboard.wrapper.client.shared;
+
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+public class RPCRequest {
+	@SuppressWarnings("unchecked")
+	public static <T> T crossdomain(Object sdt, String serviceEntryPoint) {
+		if (!(sdt instanceof ServiceDefTarget))
+			return null;
+
+		ServiceDefTarget svc = (ServiceDefTarget) sdt;
+		svc.setRpcRequestBuilder(new RpcRequestBuilder());
+		svc.setServiceEntryPoint(serviceEntryPoint);
+
+		return (T) sdt;
+	}
+
+}
