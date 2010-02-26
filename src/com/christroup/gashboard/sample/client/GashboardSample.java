823428b6da390675dca7f43d57e51afdac95f9e0
minichate <minichate@5e9b253a-1a51-11df-85e6-9b4bd7dd5138>
2010-02-26 23:12:09 +0000

    update sample widget to show off a few features
    git-svn-id: https://gashboard.googlecode.com/svn/trunk@25 5e9b253a-1a51-11df-85e6-9b4bd7dd5138

--- src/com/christroup/gashboard/sample/client/GashboardSample.java
+++ src/com/christroup/gashboard/sample/client/GashboardSample.java
@@ -4,14 +4,22 @@ import com.christroup.gashboard.wrapper.client.BackButton;
 import com.christroup.gashboard.wrapper.client.DashboardWidget;
 import com.christroup.gashboard.wrapper.client.InfoButton;
 import com.christroup.gashboard.wrapper.client.WidgetOptions;
+import com.christroup.gashboard.wrapper.client.events.WidgetDragEvent;
+import com.christroup.gashboard.wrapper.client.events.WidgetDragHandler;
+import com.christroup.gashboard.wrapper.client.events.WidgetVisibilityEvent;
+import com.christroup.gashboard.wrapper.client.events.WidgetVisibilityHandler;
 import com.christroup.gashboard.wrapper.client.shared.RPCRequest;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 
@@ -39,8 +47,13 @@ public class GashboardSample extends DashboardWidget {
 			String front();
 			String back();
 			String title();
+			String text();
 		}
 	}
+	
+	HTML rpc = new HTML("Gashboard includes a wrapper for GWT RPC. Click the button to make an RPC call.");
+	int shown = 0;
+	int hidden = 0;
 
 	protected void widgetBack(AbsolutePanel back) {
 		SampleResources.INSTANCE.css().ensureInjected();
@@ -69,12 +82,77 @@ public class GashboardSample extends DashboardWidget {
 		
 		front.add(title);
 		
-		TestServiceAsync svc = RPCRequest.crossdomain(GWT.create(TestService.class), "http://gashboard.appspot.com/sample/test");
-		svc.myTestMethod("test", new AsyncCallback<String>() {
+		rpc.setStylePrimaryName(SampleResources.INSTANCE.css().text());
+		front.add(rpc);
+		front.add(new Button("RPC Call", new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				performRPCCall();
+			}
+		}));
+		
+		HTML moveHandlers = new HTML("<br />Try moving the widget. It supports drag handlers!");
+		moveHandlers.setStyleName(SampleResources.INSTANCE.css().text());
+		final HTML moveHandlersMessage = new HTML();
+		moveHandlersMessage.setStyleName(SampleResources.INSTANCE.css().text());
+		
+		front.add(moveHandlers);
+		front.add(moveHandlersMessage);
+		
+		addWidgetDragHandler(new WidgetDragHandler() {
+			public void onDragStart(WidgetDragEvent event) {
+				moveHandlersMessage.setHTML("<strong>Dragging...</strong>");
+				
+			}
+			public void onDragEnd(WidgetDragEvent event) {
+				moveHandlersMessage.setHTML("<strong>Drag ended</strong>");
+			}
+		});
+		
+		
+		HTML visibilityHandlers = new HTML("<br />The counter below will tell you how many times the widget has been hidden/shown:");
+		visibilityHandlers.setStyleName(SampleResources.INSTANCE.css().text());
+		final HTML visibilityHandlersMessage = new HTML();
+		visibilityHandlersMessage.setStyleName(SampleResources.INSTANCE.css().text());
+		
+		front.add(visibilityHandlers);
+		front.add(visibilityHandlersMessage);
+		
+		addWidgetVisibilityHandler(new WidgetVisibilityHandler() {
+			
+			@Override
+			public void onShow(WidgetVisibilityEvent event) {
+				shown++;
+				visibilityHandlersMessage.setHTML("<strong>Shown: " + shown + " times and Hidden: " + hidden + " times</strong>");
+			}
+			
+			@Override
+			public void onHide(WidgetVisibilityEvent event) {
+				hidden++;
+				visibilityHandlersMessage.setHTML("<strong>Shown: " + shown + " times and Hidden: " + hidden + " times</strong>");
+			}
+		});
+		
+	}
+	
+	private void performRPCCall() {
+		/*
+		 * If we are running in hosted mode this will query the local service. If this is a compiled
+		 * widget it will make an RPC call to an appengine service.
+		 */
+		TestServiceAsync svc;
+		if (GWT.isScript()) {
+			svc = RPCRequest.crossdomain(GWT.create(TestService.class), "http://gashboard.appspot.com/sample/test");
+		} else {
+			svc = GWT.create(TestService.class);
+		}
+		
+		svc.myTestMethod(Window.Navigator.getUserAgent(), new AsyncCallback<String>() {
 			
 			@Override
 			public void onSuccess(String result) {
-				front.add(new Label(result));
+				rpc.setHTML(rpc.getHTML() + " <strong>" + result + "</strong> ");
 			}
 			
 			@Override
@@ -82,7 +160,6 @@ public class GashboardSample extends DashboardWidget {
 				Window.alert("fail");
 			}
 		});
-		
 	}
 	
 
